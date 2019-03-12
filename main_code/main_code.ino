d602386040647a9c2b6862e5fb94855f28f52272
Brittney Covach <blcovach@gmail.com>
2019-03-12 00:54:12 -0400

    Began to add code that completes remaining tasks after terrain has been fully explored.

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -46,6 +46,19 @@ int curr_direction_index = 0; //start facing north
 int curr_row = 5;
 int curr_col = 2;
 
+// Task numbers...
+#define FIRE_OFF          0
+#define COLLECT_FOOD      1
+#define FEED_SURVIVORS    2
+#define FIND_LOST_PERSON  3
+
+#define TOTAL_NUM_TASKS   4
+
+int task_status[4] = {0}; //All tasks are incomplete at start
+
+int task_location[4][2] = {0};
+// This is used to find exact location of each task quickly without having to iterate over the whole terrain map
+
 void setup() {
   Serial.begin(9600);
 
@@ -141,6 +154,8 @@ void Handle_Object() {
   ReadColour();
 
   // BRITT: Maybe add all these number values as global constants instead of hardcoded values?
+  // BRITT: Once task is complete, set corresponding flag in task_status[] (i.e. task_status[COLLECT_FOOD] = 1)
+  // BRITT: Also, these should be updating terrain_map[][] and task_location[][] accordingly.
   if(totalRGB > 150 && double(red+green)/totalRGB >= 0.70 && double(green) / totalRGB > 0.38) {
     Serial.println("Detect Yellow House");
   }
@@ -223,7 +238,7 @@ void ExploreTerrain() {
       Stop_Motors();
     }
 
-    //TODO: Change search direction if needed, i.e. if the outter layer has been searched now search the inner layer
+    //TODO: Change search direction if needed, i.e. if the outer layer has been searched now search the inner layer
   }
   else if(a < 5) {
     //if a wall
@@ -241,6 +256,33 @@ void ExploreTerrain() {
   }
 }
 
+void CompleteRemainingTasks() {
+  // Determine closest on-queue task that can actually be completed
+  int upcoming_task = 0;
+  int approx_distance = 26; // Max distance would be 5+5, so add 1 to make it larger than any possible distance value
+  int i;
+  
+  for (i = 0; i < TOTAL_NUM_TASKS; i++) {
+    if (task_status[i] == 0) {
+      // Task not complete yet, check if valid task...
+      // ASSUMPTION: Remember if the whole terrain has already been explored by this point, fire must already be out, skip this check
+      if (i != FEED_SURVIVORS || task_status[COLLECT_FOOD] == 1) {
+        // Valid task, check distance to task...
+        approx_distance_i = abs(task_location[i][0]-curr_row) + abs(task_location[i][1]-curr_col);
+        if (approx_distance_i < approx_distance) {
+          // Task at i is closer, update values...
+          upcoming_task = i;
+          approx_distance = approx_distance_i;
+        }
+      }
+    }
+  }
+  
+  // Use A* to go to the location
+  // Handle object
+  // If all tasks are not complete, repeat process
+}
+
 void loop() {
   digitalWrite(ENABLE_M1, HIGH);
   digitalWrite(ENABLE_M2, HIGH);
@@ -255,4 +297,5 @@ void loop() {
   //Record starting position
   terrain_map[curr_row][curr_col] = '1';
   ExploreTerrain();
+  CompleteRemainingTasks();
 }
